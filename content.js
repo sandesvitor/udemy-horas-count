7eecdea50f2764cc14bf2c3e34419ec85a65deb1
Vitor Sandes <sandesvitor@gmail.com>
2020-05-24 22:42:03 -0300

    Changed script to precisilly add time to div box

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-console.log('HoursWatched 1.0.1 - status: active') 
+console.log('HoursWatched 1.0.2 - status: active') 
 
 if (window.location.hostname == "www.udemy.com") {
     
@@ -10,70 +10,116 @@ if (window.location.hostname == "www.udemy.com") {
             
             clearInterval(interval)
             
-            window.location.hash = '#content'
-        
-            const getWatchFactor = () => {
-                let a = document.querySelectorAll('.mr-space-xxs')
+            window.location.hash = '#content'          
+
+
+            const openSections = () => {        
+                const contentsDiv = document.querySelectorAll('.section--section-heading--2k6aW')      
+                const lessonsFraction = document.querySelectorAll('.mr-space-xxs')
+                const selectionArrowUp = document.querySelectorAll('.section--section-chevron--tJ4mD.udi.udi-angle-up')
                 let numerador = 0
-                let denominador = 0
-                let arrayFator = []
-                let len = 0
+                let arrayFraction = []
             
-                for (i=0; i < a.length; i++) {
-                    arrayFator = a[i].innerText.split('/')
-                    len = arrayFator.length
             
-                    numerador += parseFloat(arrayFator[0])
-                    denominador += parseFloat(arrayFator[len-1])
+                for (i=0; i < lessonsFraction.length; i++) {
             
-                }
+                    arrayFraction = lessonsFraction[i].innerText.split('/')
+                    numerador = parseFloat(arrayFraction[0])                        
             
-                let factor = numerador/denominador
             
-                return factor
-            }        
-
-
-            const getTempoTotal = () => {
-                let minutos = 0
-                let horas = 0
-                let a = document.querySelectorAll('.ml-space-xxs');
-            
-                for (let i = 0; i < a.length - 1; i++) {
-            
-                    let dom = a[i].innerText.split('')
-            
-                    if (dom.includes('h')) {
-                        horas += parseFloat(dom.slice(0,1))
-                        minutos += parseFloat(dom.slice(3, dom.length - 1).join(''))
-                    } else {
-                        minutos = minutos + parseFloat(dom.join(''))
+                    if (!selectionArrowUp[i] && numerador !== 0){
+                       contentsDiv[i].click()                                                                 
                     }
                 }
-            
-                let horasTotais = (horas*60 + minutos)/60 
-                return horasTotais
+            } 
+
+            const getWatchedTime = () => {
+                let totalTime = 0
+              
+                let lessons = document.querySelectorAll('.mr-space-xxs')
+                let time = document.querySelectorAll('.ml-space-xxs')         
+                const innerSectionDiv = document.querySelectorAll('.curriculum-item-link--lecture-type-resource-container--2l5ZE')
+                const sectionCheckbox = document.querySelectorAll('.curriculum-item-link--progress-toggle--1CMcg.checkbox-inline input') 
+        
+                let arrayLessons = []
+                let lessonsWatched = 0
+                let lessonsWatchedInit = 0
+                let lessonsTotal = 0
+                let lessonsTotalSum = 0
+                let arrayTime = []
+        
+                let horas = 0
+                let minutos = 0
+                let sectionMinutes = 0
+        
+        
+                for (i=0; i < lessons.length; i++) {
+                    horas = 0
+                    minutos = 0
+                    sectionMinutes = 0
+                    arrayLessons = []
+                    lessonsWatched = 0
+                    lessonsTotal = 0
+                    arrayTime = []
+        
+                    arrayLessons = lessons[i].innerText.split('/')
+                    lessonsWatched = parseFloat(arrayLessons[0])
+                    lessonsTotal = parseFloat(arrayLessons[1])
+        
+                    arrayTime = time[i].innerText.split('')
+                    if (arrayTime.includes('h')) {
+                        horas = parseFloat(arrayTime.slice(0,1))
+                        minutos = parseFloat(arrayTime.slice(3, arrayTime.length - 1).join(''))
+                    } else {
+                        minutos = minutos + parseFloat(arrayTime.join(''))
+                    }
+                    
+        
+                    lessonsTotalSum += lessonsTotal
+                     
+                    if (lessonsWatched === lessonsTotal){
+                        sectionMinutes = horas*60 + minutos      
+                    } else if (lessonsWatched == 0) {
+                        sectionMinutes = 0
+                    } else {
+                       for (let j = lessonsWatchedInit; j < lessonsTotalSum; j++){                   
+                           if (sectionCheckbox[j].checked){
+                           sectionMinutes += parseFloat(innerSectionDiv[j].innerText)
+                           }
+                       }
+                    }
+                     
+                    lessonsWatchedInit += lessonsTotal 
+                    
+                    totalTime += sectionMinutes                                                                     
+                }                    
+        
+              
+        
+                return totalTime
             }
-            
+        
+        
+           
             
 
             document.querySelector('.progress--progress-container--RxDGm.popper--click-target--caIsH').addEventListener("click", () => {
    
                 if (!document.querySelector('.popover--popover--click--3nXVQ')){
-                     window.location.hash = "#content"
+                    window.location.hash = "#content"
                 }
             
-                const box_interval = setTimeout( ()=> {
+                setTimeout( ()=> {
                     
                     let box = document.querySelector('.popover--popover--click--3nXVQ')
             
                     if (box) {
                         
-                        clearInterval(box_interval)
+                        openSections()
 
-                        let horasAssistidas = (getWatchFactor() * getTempoTotal()).toFixed(2)
-                        let hrs = parseInt(horasAssistidas)
-                        let min = parseInt((horasAssistidas - hrs) * 60)
+                        let minutosAssistidos = getWatchedTime()
+                        let hrs = parseInt(minutosAssistidos / 60)
+                        let min = parseInt(minutosAssistidos % 60)
             
 
                         let div_header = document.createElement('div')                        
